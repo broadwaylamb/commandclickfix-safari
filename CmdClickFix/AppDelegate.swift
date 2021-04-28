cf4cc71c2e9deb24b97fc8b3845825bb05574acf
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2021-04-28 14:49:30 +0300

    Initial Commit

---
+++ CmdClickFix/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CmdClickFix
+//
+//  Created by Sergej Jaskiewicz on 28.04.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
